fa039cb303d1ae58aaa1cc5f0ffc1f3b9361ed57
Adam Mihalik <adam.mihalik@panaxeo.com>
2023-08-14 14:16:31 +0200

    running android app

---
+++ ExponeaSDKMauiIOS/ExponeaSDKMauiIOS/ExponeaSDKMauiIOS.h
@@ -0,0 +1,8 @@
+//! Project version number for ExponeaSDKMauiIOS.
+FOUNDATION_EXPORT double ExponeaSDKMauiIOSVersionNumber;
+
+//! Project version string for ExponeaSDKMauiIOS.
+FOUNDATION_EXPORT const unsigned char ExponeaSDKMauiIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExponeaSDKMauiIOS/PublicHeader.h>
+
