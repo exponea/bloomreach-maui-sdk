02f76214dfeb683ea324df2c67c2c1eb216b1217
adam1929 <103995671+adam1929@users.noreply.github.com>
2023-09-11 14:13:04 +0200

    Removes obsolete Android method channel consumer

--- ExponeaSDK/Platforms/Android/MethodChannelConsumerAndroid.cs
+++ ExponeaSDK/Platforms/Android/MethodChannelConsumerAndroid.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Text.Json;
+using Com.Exponea.Sdk.Maui.Android;
 
 namespace Exponea.Platforms.Android
 {
@@ -22,12 +23,60 @@ namespace Exponea.Platforms.Android
 
         void IMethodChannelConsumerPlatformSpecific.InvokeMethodAsync(string method, string? data, Action<MethodMauiResult, Exception?> action)
         {
-            throw new NotImplementedException();
+            try
+            {
+                NativeSdk.InvokeMethodAsync(method, data, new KotlinCallback<MethodResult>((nativeResult) =>
+                {
+                    try
+                    {
+                        var mauiResult = new MethodMauiResult(
+                            nativeResult.Success,
+                            nativeResult.Data,
+                            nativeResult.Error
+                        );
+                        action.Invoke(mauiResult, null);
+                    }
+                    catch (Exception e)
+                    {
+                        action.Invoke(
+                            new MethodMauiResult(false, "", $"Native {method} failed, see logs"),
+                            e
+                        );
+                    }
+                }));
+            }
+            catch (Exception e)
+            {
+                action.Invoke(
+                    new MethodMauiResult(false, "", $"Native {method} failed, see logs"),
+                    e
+                );
+            }
         }
 
         MethodMauiResultForView IMethodChannelConsumerPlatformSpecific.InvokeUiMethod(string method, string? data)
         {
             throw new NotImplementedException();
+        }
+    }
+    
+    internal class KotlinCallback<TArg> : Java.Lang.Object, Kotlin.Jvm.Functions.IFunction1
+        where TArg: Java.Lang.Object?
+    {
+        private readonly Action<TArg> _callback;
+
+        public KotlinCallback(Action<TArg> callback)
+        {
+            _callback = callback;
+        }
+
+        public Java.Lang.Object? Invoke(Java.Lang.Object? p0)
+        {
+            if (p0 is TArg result)
+            {
+                _callback.Invoke(result);
+            }
+            return null;
         }
     }
 }
