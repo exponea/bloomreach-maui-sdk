5d9a3e601318bdbd9c869d7eb39e8fad7ac191e5
adam1929 <103995671+adam1929@users.noreply.github.com>
2023-09-14 16:31:48 +0200

    Version 0.1.0

--- BloomreachSDK/Properties/AssemblyInfo.cs
+++ BloomreachSDK/Properties/AssemblyInfo.cs
@@ -17,13 +17,13 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 #if ANDROID
 [assembly: Android.App.MetaData("BloomreachMauiSDK", Value = "true")]
-[assembly: Android.App.MetaData("BloomreachMauiSDKVersion", Value = "0.0.1")]
+[assembly: Android.App.MetaData("BloomreachMauiSDKVersion", Value = "0.1.0")]
 #endif
--- Documentation/RELEASE_NOTES.md
+++ Documentation/RELEASE_NOTES.md
@@ -1,1 +1,7 @@
 ## Release Notes
+
+## Release Notes for 0.1.0
+#### September 14, 2023
+* Features
+    * Added base API
+    * Added tracking API
